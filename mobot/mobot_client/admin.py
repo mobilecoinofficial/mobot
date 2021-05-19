1f4b89d4a3a13654c952ab35bc1797f04c7d8459
Brian Corbin <brian.william.corbin@gmail.com>
2021-05-19 16:51:17 -0400

    Getting ready for first drop

--- mobot/mobot_client/admin.py
+++ mobot/mobot_client/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Store, Customer, Drop, Item, CustomerStorePreferences, DropSession, Message
 
-# Register your models here.
+class StoreAdmin(admin.ModelAdmin):
+    pass
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+class DropAdmin(admin.ModelAdmin):
+    pass
+
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+class CustomerStorePreferencesAdmin(admin.ModelAdmin):
+    pass
+
+class DropSessionAdmin(admin.ModelAdmin):
+    pass
+
+class MessageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Store, StoreAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Drop, DropAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(CustomerStorePreferences, CustomerStorePreferencesAdmin)
+admin.site.register(DropSession, DropSessionAdmin)
+admin.site.register(Message, MessageAdmin)
