0f84ac465532159b715000c4981fa5569378e7cb
Brian Corbin <brian.william.corbin@gmail.com>
2021-05-21 13:20:19 -0400

    removed customer name field (#11)

--- mobot/mobot_client/models.py
+++ mobot/mobot_client/models.py
@@ -36,7 +36,6 @@ class Drop(models.Model):
 
 class Customer(models.Model):
     phone_number = models.TextField(primary_key=True)
-    name = models.TextField(default=None, null=True, blank=True)
     received_sticker_pack = models.BooleanField(default=False)
     
     def __str__(self):
