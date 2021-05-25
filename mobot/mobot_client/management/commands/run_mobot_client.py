0b9844e5a2e325cf5ca09f03b38c96dd4ea0addf
Brian Corbin <brian.william.corbin@gmail.com>
2021-05-25 17:08:43 -0400

    Added logic for making a payment and refunding the customer in FS

--- mobot/mobot_client/management/commands/run_mobot_client.py
+++ mobot/mobot_client/management/commands/run_mobot_client.py
@@ -1,16 +1,27 @@
 from datetime import tzinfo
 import os
+import time
 import pytz
 
 from django.utils import timezone
 from django.core.management.base import BaseCommand
 from signald_client import Signal
 from mobot_client.models import Store, Customer, DropSession, Drop, CustomerStorePreferences, Message
+import mobilecoin as mc
+from decimal import Decimal
 
 SIGNALD_ADDRESS = os.getenv("SIGNALD_ADDRESS", "127.0.0.1")
 SIGNALD_PORT = os.getenv("SIGNALD_PORT", "15432")
 STORE_NUMBER = os.environ["STORE_NUMBER"]
 signal = Signal(STORE_NUMBER, socket_path=(SIGNALD_ADDRESS, int(SIGNALD_PORT)))
+
+FULLSERVICE_ADDRESS = os.getenv("FULLSERVICE_ADDRESS", "127.0.0.1")
+FULLSERVICE_PORT = os.getenv("FULLSERVICE_PORT", "9090")
+FULLSERVICE_URL = f"http://{FULLSERVICE_ADDRESS}:{FULLSERVICE_PORT}/wallet"
+mcc = mc.Client(url=FULLSERVICE_URL)
+
+PUBLIC_ADDRESS = os.environ["PUBLIC_ADDRESS"]
+ACCOUNT_ID = os.environ["ACCOUNT_ID"]
 
 store = Store.objects.get(phone_number=STORE_NUMBER)
 
@@ -21,7 +32,107 @@ SESSION_STATE_ALLOW_CONTACT_REQUESTED = 1
 MESSAGE_DIRECTION_RECEIVED = 0
 MESSAGE_DIRECTION_SENT = 1
 
-signal.set_profile("MOBot", None, "avatar.png", False)
+signal.set_profile("MOBot - Local Brian", PUBLIC_ADDRESS, None, False)
+
+def _signald_to_fullservice(r):
+    return {
+        "object": "receiver_receipt",
+        "public_key": r['txo_public_key'],
+        "confirmation": r['txo_confirmation'],
+        "tombstone_block": str(r['tombstone']),
+        "amount": {
+            "object": "amount",
+            "commitment": r['amount_commitment'],
+            "masked_value": str(r['amount_masked'])
+        }
+    }
+
+def get_payments_address(source):
+    customer_signal_profile = signal.get_profile(source, True)
+    customer_payments_address = customer_signal_profile['data']['paymentsAddress']
+    if customer_payments_address is None:
+        return None
+    return customer_payments_address
+
+def refund_customer(source, amount_mob, cover_transaction_fee):
+    customer_payments_address = get_payments_address(source)
+    if customer_payments_address is None:
+        signal.send_message(source,
+                       ("We have a refund for you, but your payments have been deactivated\n\n"
+                       "Please contact customer service at {}").format(STORE_NUMBER))
+        return
+    
+    if not cover_transaction_fee:
+        amount_mob = amount_mob - Decimal(0.01)
+
+    if amount_mob <= 0:
+        signal.send_message(source, "Sorry. Can't issue a refund because the refund amount is less than the transaction fee 🙁")
+        return
+
+    send_mob_to_user(source, ACCOUNT_ID, amount_mob, customer_payments_address)
+
+def send_mob_to_user(source, account_id, amount_in_mob, customer_payments_address):
+    tx_proposal = mcc.build_transaction(account_id, amount_in_mob, customer_payments_address)
+    txo_id = submit_transaction(tx_proposal, account_id)
+    for _ in range(10):
+        try:
+            mcc.get_txo(txo_id)
+            break
+        except Exception:
+            print("TxOut did not land yet, id: " + txo_id)
+            pass
+        time.sleep(1.0)
+    else:
+        signal.send_message(source, "couldn't generate a receipt, please contact us if you didn't get a refund!")
+        return
+    
+    send_payment_receipt(source, tx_proposal)
+    signal.send_message(source, "{} MOB refunded".format(float(amount_in_mob)))
+
+def submit_transaction(tx_proposal, account_id):
+    transaction_log = mcc.submit_transaction(tx_proposal, account_id)
+    list_of_txos = transaction_log["output_txos"]
+
+    # I'm assuming there will only be one tx out (not including change tx out).
+    if len(list_of_txos) > 1:
+        raise ValueError("Found more than one txout for this chat bot-initiated transaction.")
+
+    return list_of_txos[0]["txo_id_hex"]
+
+def send_payment_receipt(source, tx_proposal):
+    receiver_receipt = create_receiver_receipt(tx_proposal)
+    print("sending payment receipt")
+    print(receiver_receipt)
+    signal.send_payment_receipt(source, receiver_receipt, "Refund")
+
+def create_receiver_receipt(tx_proposal):
+    receiver_receipts = mcc.create_receiver_receipts(tx_proposal)
+    # I'm assuming there will only be one receiver receipt (not including change tx out).
+    if len(receiver_receipts) > 1:
+        raise ValueError("Found more than one txout for this chat bot-initiated transaction.")
+    return receiver_receipts[0]
+
+@signal.chat_handler("balance")
+def account_balance(message, match):
+    account_balance_response = mcc.get_balance_for_account(ACCOUNT_ID)
+    unspent_pmob = account_balance_response['unspent_pmob']
+    unspent_mob = mc.pmob2mob(unspent_pmob)
+    return f'You have {unspent_mob} unspent MOB'
+
+@signal.payment_handler
+def handle_payment(source, receipt):
+    receipt_status = None
+    transaction_status = "TransactionPending"
+
+    while transaction_status == "TransactionPending":
+        receipt_status = mcc.check_receiver_receipt_status(PUBLIC_ADDRESS, _signald_to_fullservice(receipt))
+        transaction_status = receipt_status["receipt_transaction_status"]
+    
+    if transaction_status != "TransactionSuccess":
+        return "The transaction failed!"
+
+    amount_paid_mob = mc.pmob2mob(receipt_status["txo"]["value_pmob"])
+    refund_customer(source, amount_paid_mob, False)
 
 # catch all chat handler, will perform our own routing from here
 @signal.chat_handler("")
@@ -130,6 +241,7 @@ def get_signal_profile_name(source):
 
 def get_payments_address(source):
     customer_signal_profile = signal.get_profile(source, True)
+    print(customer_signal_profile)
     try:
         customer_payments_address = customer_signal_profile['data']['paymentsAddress']
         return customer_payments_address
