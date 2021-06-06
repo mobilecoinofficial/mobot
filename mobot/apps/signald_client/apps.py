3490b4013bbc08efa349b535fa5de1f6884676e6
Gregory Rice <greg@mobilecoin.com>
2021-06-06 15:17:11 -0700

    Django stuff solved, back to full-service wallet connection issues

---
+++ mobot/apps/signald_client/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class SignalClientAppConfig(AppConfig):
+    name = 'apps.signald_client'
+    default_auto_field = 'django.db.models.BigAutoField'
+    verbose_name = 'Signal client app config'
