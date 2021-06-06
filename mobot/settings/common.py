1512b8790db1dbe80331de435de559836103f822
Gregory Rice <greg@mobilecoin.com>
2021-06-06 15:17:11 -0700

    Django stuff solved, back to full-service wallet connection issues

--- mobot/settings/common.py
+++ mobot/settings/common.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.merchant_services.apps.MerchantServicesConfig',
     'apps.mobot_client',
-    'apps.common',
+    'apps.common.apps.CommonAppConfig'
 ]
 
 MIDDLEWARE = [
