cafb6c5dd629f0aa8dfceeef2e7a699855414698
Gregory Rice <greg@mobilecoin.com>
2021-06-24 15:33:32 -0700

    More validations work

--- mobot/apps/merchant_services/models.py
+++ mobot/apps/merchant_services/models.py
@@ -2,26 +2,96 @@ import datetime
 import importlib
 
 from django.db import models
+from django.db.models import QuerySet
+from django.contrib.auth.models import User
 from django.db.models import Q
+from mobot.apps.common.models import ClassField
 from phonenumber_field.modelfields import PhoneNumberField
 from djmoney.models.fields import MoneyField
-from django.contrib.postgres.fields import ArrayField
+from typing import TypeVar, List
+from moneyed import Money
 from mobot.apps.signald_client import Signal
 from mobot.apps.payment_service.models import Payment
+import logging
+from itertools import chain
+import inspect
+from functools import singledispatchmethod
+from django.utils import timezone as tz
+
+T = TypeVar('T', bound=models.Model)
 
 
+class ValidationTargetManager(models.Manager):
+    def _get_queryset(self, validations, base_condition: Q = Q(), target: models.Model = None):
+        for validation in validations:
+            try:
+                logging.warning(f"Adding filter: {validation.q}")
+                base_condition.add(validation.q, Q.AND)
+            except AttributeError as e:
+                logging.exception("Can't get attr")
+        return target.objects.filter(base_condition)
 
-class UserAccount(models.Model):
-    phone_number = PhoneNumberField(primary_key=True)
-    name = models.TextField()
+    def get_targets(self, validations, campaign_id: str = ""):
+        qs = self._get_queryset(validations, target=Customer)
+        return qs
 
-    class Meta:
-        abstract = True
-        app_label = 'merchant_services'
-        ordering = ['name']
+class Validation(models.Model):
+    model_class_name = models.TextField(db_index=True, blank=False, null=False)
+    model_attribute_name = models.TextField(blank=False, null=False)
+    comparator_func = models.TextField(blank=False, null=False)
+    target_value = models.TextField(blank=False, null=False)
+    objects = models.Manager()
+
+    def clean(self):
+        if not self.model_class_name.startswith("mobot"):
+            self.model_class_name = f"mobot.apps.merchant_services.models.{self.model_class_name}"
+
+    @property
+    def _model_class(self):
+        try:
+            module = importlib.import_module(f"mobot.apps.merchant_services.models")
+            return getattr(module, self.model_class_name)
+        except ModuleNotFoundError as e:
+            logging.exception(f"Unable to find module {__file__}")
+            raise e
+
+    @property
+    def q(self) -> Q:
+        q = Q(**{f"{self.model_attribute_name}__{self.comparator_func}": self.target_value})
+        return q
 
     def __str__(self):
-        return f'{self.phone_number}'
+        return f"{self._model_class}::{self.model_attribute_name}{self.comparator_func}={self.target_value}"
+
+class Trackable(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    created_by = models.ForeignKey(User, related_name='created_%(class)s', on_delete=models.DO_NOTHING, null=True, blank=True)
+    updated_by = models.ForeignKey(User, related_name='updated_%(class)s', on_delete=models.DO_NOTHING, null=True, blank=True)
+    class Meta:
+        abstract = True
+
+class ValidatableMixin(Trackable):
+    class Meta:
+        abstract = True
+
+    targets = ValidationTargetManager()
+    objects = models.Manager()
+    validations = models.ManyToManyField(Validation)
+
+    @classmethod
+    def get_targets(cls, model_id, model_class_name=""):
+        validations = cls.objects.filter(id=model_id).first().validations.filter(model_class_name=model_class_name).all()
+        cls.targets.get_targets(validations=validations, campaign_id=model_id)
+
+
+class UserAccount(ValidatableMixin):
+    phone_number = PhoneNumberField(primary_key=True)
+    name = models.TextField(blank=False, null=False)
+
+    class Meta(ValidatableMixin.Meta):
+        app_label = 'merchant_services'
+        ordering = ['name']
 
     def signal_payments_enabled(self, signal: Signal) -> bool:
         signal_profile = signal.get_profile_from_phone_number(self.phone_number)
@@ -37,120 +107,106 @@ class UserAccount(models.Model):
             customer_payments_address = signal_profile['data']['paymentsAddress']
             return customer_payments_address
         except Exception as e:
-            return None
+             logging.exception(str(f"{self.name}:{self.phone_number}"))
 
 
 class Merchant(UserAccount):
     class Meta(UserAccount.Meta):
         db_table = 'merchant_service_users'
     merchant_description = models.TextField(blank=True, default="A Mobot Merchant")
-
+    objects = models.Manager()
 
 
 class Customer(UserAccount):
-    class Meta(UserAccount.Meta):
+    class Meta(ValidatableMixin.Meta):
         db_table = 'merchant_service_customers'
+
     received_sticker_pack = models.BooleanField(default=False)
+    objects = models.Manager()
 
-
-class MCStore(models.Model):
+class MCStore(Trackable):
     name = models.TextField(blank=True, default="Coin Shop")
     description = models.TextField(blank=True, default="Mobot Store")
     privacy_policy_url = models.URLField(blank=True, default="https://mobilecoin.com/privacy")
     merchant_ref = models.ForeignKey(Merchant, on_delete=models.CASCADE, related_name="store_owner")
 
     def __str__(self):
-        return f'{self.name} ({self.phone_number})'
+        return f'{self.name}:{self.phone_number}:store'
 
 
-
-class Product(models.Model):
+class Product(Trackable):
     store_ref = models.ForeignKey(MCStore, on_delete=models.CASCADE)
     name = models.TextField(blank=False, null=False)
-    description = models.TextField(default=None, blank=True, null=True)
-    short_description = models.TextField(default=None, blank=True, null=True)
+    description = models.TextField(default="Airdrop: Free mobilecoin!", blank=True, null=True)
+    short_description = models.TextField(default="Airdrop", blank=False, null=False)
     image_link = models.URLField(default=None, blank=True, null=True)
-    allows_refund = models.BooleanField(default=True, blank=False)
+    allows_refund = models.BooleanField(default=True, blank=False, null=False)
     price = MoneyField(max_digits=14, decimal_places=5, default_currency='GBP', help_text='Price of the product',
                        blank=False, default=1.0)
 
     class Meta:
         app_label = 'merchant_services'
-        abstract = True
-
-    def __str__(self):
-        return f'{self.store.name} - {self.name} - {self.price}'
+        abstract = False
+        default_related_name = 'products'
 
 
-class Drop(Product):
+class InventoryItem(models.Model):
+    product_ref = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="inventory")
+
+
+class Campaign(ValidatableMixin):
+    name = models.TextField(help_text="Campaign name", null=False, blank=False)
+    product_ref = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="campaigns", db_index=True)
     pre_drop_description = models.TextField(default="MobileCoin Product Drop")
-    advertisement_start_time = models.DateTimeField(default=datetime.datetime.utcnow())
-    start_time = models.DateTimeField(default=datetime.datetime.utcnow())
-    end_time = models.DateTimeField(default=datetime.datetime.utcnow() + datetime.timedelta(days=1))
+    advertisement_start_time = models.DateTimeField(auto_now=True)
+    start_time = models.DateTimeField(default=tz.now())
+    end_time = models.DateTimeField(default=tz.now() + datetime.timedelta(days=1))
     quota = models.PositiveIntegerField(default=10)
+    adjusted_price = MoneyField(max_digits=14, default=None, decimal_places=5, default_currency="PMB", blank=True, null=True)
 
-    def __str__(self):
-        return f'Store: {self.store_ref.name} - Name: {self.name} - Price: {self.price} - Start: {self.start_time} - Remaining: {self.quota}'
+    def _get_targets(self, model_class):
+        return Campaign.get_targets(self.id, model_class_name=model_class)
 
+    def get_target_validations(self, model_class):
+        campaign_validations = self._get_targets(model_class)
+        return campaign_validations
 
-class Sale(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=False, null=False)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=False, null=False)
-    price = MoneyField(blank=False, null=False)
+    def get_target_customers(self):
+        customer_validations = self._get_targets(Customer.__name__)
+        return customer_validations
 
     @property
-    def merchant(self) -> Merchant:
-        return self.product.store_ref.merchant_ref
+    def price(self) -> Money:
+        if self.adjusted_price:
+            return self.adjusted_price
+        else:
+            return self.product_ref.price
+
+    def __str__(self):
+        return str(f"Campaign({self.id}, {self.name})")
 
 
-class CampaignTargetManager(models.Manager):
-    def get_queryset(self):
-        base_condition = Q()
-        for validation in self.model.validation_set.target_validations:
-            base_condition.add(validation.q, Q.AND)
-        return Customer.objects.filter(base_condition)
+class Sale(Trackable):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=False, null=False, related_name="sales")
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=False, null=False)
+    price = MoneyField(blank=False, null=False, max_digits=14)
+    merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE, related_name="sales")
+    campaign = models.ForeignKey(Campaign, on_delete=models.DO_NOTHING, related_name="sales")
+
+    def clean(self):
+        if not self.merchant:
+            self.merchant = self.product.store_ref.merchant_ref
+        self.save()
 
 
 
-class Campaign(models.Model):
-    product_ref = models.ForeignKey(Product, on_delete=models.CASCADE)
-    targets = CampaignTargetManager()
-
-
-class Validation(models.Model):
-    fieldname = models.TextField(blank=False, null=False)
-    comparator_func = models.TextField(blank=False, null=False)
-    target_value = models.TextField(blank=False, null=False)
-    campaign_ref = models.ForeignKey(Campaign, on_delete=models.CASCADE)
-
-    def q(self) -> Q:
-        return Q(**{f"{self.fieldname}{self.comparator_func}": self.target_value})
-
-
-class CustomerStorePreferences(models.Model):
+class CustomerStorePreferences(Trackable):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     store_ref = models.ForeignKey(MCStore, on_delete=models.CASCADE)
     allows_contact = models.BooleanField()
-    allows_payment = models.BooleanField()
 
 
-class DropSession(models.Model):
-
-    class SessionState(models.IntegerChoices):
-        COMPLETED = -1
-        STARTED = 0
-        ALLOW_CONTACT_REQUESTED = 1
-        EXPIRED = 2
-
-    adjusted_price_pmob = models.PositiveIntegerField(blank=True, null=True, help_text="If we want to adjust the price to a target GBP, this is where it goes")
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    drop_ref = models.ForeignKey(Drop, on_delete=models.CASCADE)
-    state = models.IntegerField(default=0, choices=SessionState.choices)
-    payment_ref = models.ForeignKey(Payment, blank=True, on_delete=models.CASCADE)
-    refund = models.ForeignKey(Payment, blank=True, on_delete=models.CASCADE, related_name='refund')
-
-
-class Message(models.Model):
+class Message(Trackable):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     store_ref = models.ForeignKey(MCStore, on_delete=models.CASCADE)
     text = models.TextField()
--- mobot/apps/merchant_services/tests/model_tests.py
+++ mobot/apps/merchant_services/tests/model_tests.py
@@ -1,19 +1,39 @@
-from django.test import TestCase
+import logging
+
+from django.test import TestCase, override_settings
+from unittest.mock import Mock
 from typing import List
 import datetime
-from moneyed import Money
+from moneyed import Money, GBP
 from decimal import Decimal
-from moneyed import GBP
-from phonenumbers import PhoneNumber
-import phonenumbers
-from mobot.apps.merchant_services.models import Customer, Merchant, MCStore, Drop, Campaign, Validation
+from mobot.apps.merchant_services.models import Customer, Merchant, MCStore, Campaign, Validation, Sale, InventoryItem, Product
+from django.db import transaction
+from django.conf import settings
+from django.utils import timezone as tz
 
 
+@override_settings(DEBUG=True, TEST=True)
 class CustomerTestCase(TestCase):
 
-    def add_default_customer(self, phone_number: PhoneNumber, name: str ="A Test Customer") -> Customer:
-        c, created = Customer.objects.get_or_create(name=name, phone_number=phone_number)
-        return c
+    def __init__(self, *args, **kwargs):
+        super(CustomerTestCase, self).__init__(*args, **kwargs)
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+    def teardDown(self) -> None:
+        Customer.objects.all().delete()
+        Merchant.objects.all().delete()
+        Campaign.objects.all().delete()
+        MCStore.objects.all().delete()
+        Validation.objects.all().delete()
+
+    def add_default_customer(self, name: str, phone_number: str = "+18054412653") -> Customer:
+        try:
+            c = Customer.objects.create(phone_number=phone_number, name=name)
+            c.save()
+            if c:
+                return c
+        except TypeError as e:
+            self.logger.exception(f"{name}:{phone_number}")
 
     def add_default_store(self, merchant: Merchant) -> MCStore:
         s, created = MCStore.objects.get_or_create(merchant_ref=merchant, name="Test MobileCoin Coin Drop Store")
@@ -21,45 +41,68 @@ class CustomerTestCase(TestCase):
         return s
 
     def add_default_merchant(self) -> Merchant:
-        m, created = Merchant.objects.get_or_create(name="Test MobileCoin Official Merchant", phone_number="+44 07911 123456")
+        m, created = Merchant.objects.get_or_create(name="Test MobileCoin Official Merchant", phone_number="447441433907")
         m.save()
         return m
 
-    def add_default_drops(self, store: MCStore) -> List[Drop]:
-        original_drop, _ = Drop.objects.update_or_create(name="Test AirDrop",
+    def _add_airdrop_product(self, price: Money):
+        product = Product(name=f"MobileCoin Airdrop - {price}", price=price, description=f"A MOB giveaway: {price}", store_ref=self.store)
+        product.save()
+        return product
+
+    def add_default_campaigns(self, store: MCStore) -> List[Campaign]:
+        airdrop_product = self._add_airdrop_product(Money(-3.0, GBP))
+        airdrop_product.save()
+
+        original_drop = Campaign.objects.create(
+                            name="Test AirDrop",
+                            product_ref=airdrop_product,
                             pre_drop_description="Get free MOB from MobileCoin!",
-                            store_ref=store,
-                            description="Test Airdrop 3.0 GBP",
-                            number_restriction=["+44"],
-                            advertisement_start_time=datetime.datetime.utcnow(),
-                            start_time=datetime.datetime.utcnow(),
-                            end_time=datetime.datetime.utcnow() + datetime.timedelta(days=3.0),
-                            price=Money(Decimal(3.0), GBP),
+                            advertisement_start_time=tz.make_aware(datetime.datetime.now(), tz.get_current_timezone()),
+                            start_time=tz.make_aware(datetime.datetime.now(), tz.get_current_timezone()),
+                            end_time=tz.make_aware(datetime.datetime.now() + datetime.timedelta(days=3.0), tz.get_current_timezone()),
+                            adjusted_price=Money(-3.0, "GBP"),
                             quota=100)
 
-
-        bonus_drops = [Drop.objects.update_or_create(name=f"Test Bonus AirDrop {price}",
-                            pre_drop_description="Get free MOB from MobileCoin!",
-                            store_ref=store,
-                            description=f"Test Bonus Drop {price}",
-                            number_restriction=["+44"],
-                            advertisement_start_time=datetime.datetime.utcnow(),
-                            start_time=datetime.datetime.utcnow(),
-                            end_time=datetime.datetime.utcnow() + datetime.timedelta(days=3.0),
+        bonus_drops = [Campaign.objects.create(
+                            name=f"Test Bonus AirDrop {product.price}",
+                            pre_drop_description=f"Test Bonus Drop {product.price}",
+                            product_ref=product,
+                            advertisement_start_time=tz.make_aware(datetime.datetime.now(), tz.get_current_timezone()),
+                            start_time=tz.make_aware(datetime.datetime.now(), tz.get_current_timezone()),
+                            end_time=tz.make_aware(datetime.datetime.now() + datetime.timedelta(days=3.0), tz.get_current_timezone()),
                             quota=quota,
-                            price=Money(Decimal(price), GBP)) for quota, price in [(80, 2.0), (10, 7.0), (7, 22.0), (3, 47)]]
+                            adjusted_price=product.price) for quota, product in [(80, self._add_airdrop_product(2.0)), (10, self._add_airdrop_product(7.0)), (7, self._add_airdrop_product(22.0)), (3, self._add_airdrop_product(47))]]
 
         return original_drop, bonus_drops
+
+    def _test_add_customer_validation(self, campaign: Campaign):
+        validation = Validation(model_class_name=Customer.__name__, model_attribute_name="phone_number", comparator_func="startswith", target_value="+44")
+        validation.save()
+        campaign.validations.add(validation)
+        return validation
 
     def setUp(self):
         self.merchant = self.add_default_merchant()
         self.store = self.add_default_store(self.merchant)
-        self.cust_us = self.add_default_customer(name="Greg", phone_number=phonenumbers.parse("+18054412653"))
-        self.cust_uk = self.add_default_customer(name="Adam", phone_number=phonenumbers.parse("+44 07911 654321"))
-        self.original_drop, self.bonus_drops = self.add_default_drops(self.store)
+        self.cust_us = self.add_default_customer("Greg", phone_number="+18054412653")
+        self.cust_uk = self.add_default_customer("Adam", phone_number="+447441433906")
+        self.original_drop, self.bonus_drops = self.add_default_campaigns(self.store)
 
     def test_can_create_drop_session(self):
         greg = Customer.objects.get(name="Greg")
         adam = Customer.objects.get(name="Adam")
         print(greg)
         print(adam)
+
+    def test_can_add_44_validation(self):
+        original_drop: Campaign = self.original_drop
+        validation = self._test_add_customer_validation(original_drop)
+        validation.save()
+        print(validation)
+        original_drop.validations.add(validation)
+        original_drop.save()
+        print(original_drop.validations)
+        targets = original_drop.get_target_customers()
+
+        print(targets)
