186583167adebd71b32c9c0471946686e46263d1
Greg Rice <greg@mobilecoin.com>
2021-07-06 08:59:54 -0700

    Cleanup

--- mobot/apps/merchant_services/models/shipment/__init__.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-from address.models import AddressField, Address
-from django.db import models
-from django_fsm import FSMIntegerField, transition
-from mobot.apps.common.models import Trackable
-
