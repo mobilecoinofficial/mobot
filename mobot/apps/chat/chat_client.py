b76a7832475df4bc9d7fe4a52d73ef5124e380b3
Greg Rice <gregrice@gmail.com>
2021-07-13 15:26:41 -0700

    Find and greet customers

--- mobot/apps/chat/chat_client.py
+++ mobot/apps/chat/chat_client.py
@@ -21,7 +21,7 @@ from mobot.signald_client import Signal
 from mobot.lib.signal import SignalCustomerDataClient
 from mobot.signald_client.types import Message as SignalMessage
 from mobot.signald_client.main import QueueSubscriber
-from mobot.apps.chat.context import MessageContextFactory
+from mobot.apps.chat.context import MessageContextFactory, MessageContextBase
 
 class TransactionStatus(str, Enum):
     TRANSACTION_SUCCESS = "TransactionSuccess"
@@ -37,13 +37,12 @@ class MobotMessage(str, Enum):
 
 
 class Mobot:
-    def __init__(self, signal: Signal, store: Store, campaigns: Set[Campaign], mobilecoin_client: mobilecoin.Client):
+    def __init__(self, signal: Signal, store: Store, campaign: Campaign, mobilecoin_client: mobilecoin.Client):
         self.logger = logging.getLogger(f"Mobot-{self.store.id}")
         self.signal = signal
-        self.campaigns = campaigns
+        self.campaign = campaign
         self.store = store
         self.mobilecoin_client = mobilecoin_client
-
         self.mobot: MobotBot = self.get_mobot_bot()
         self.message_context_factory = MessageContextFactory(self.mobot, self.logger)
         self.customer_data_client = SignalCustomerDataClient(signal=self.signal)
@@ -53,45 +52,41 @@ class Mobot:
         self._chat_handlers = []
         self.register_handlers()
 
+    def get_context_from_message(self, message: SignalMessage) -> MessageContextBase:
+        context: MessageContextBase = self.message_context_factory.get_message_context(message)
+        return context
+
+    def get_context_from_customer(self, customer: Customer) -> MessageContextBase:
+        context = self.message_context_factory.get_message_context(message=None, customer=customer)
+        return context
+
     def get_mobot_bot(self) -> MobotBot:
-        created, _ = MobotBot.objects.get_or_create(name=self.name, store=self.store)
+        bot, _ = MobotBot.objects.get_or_create(name=self.name, store=self.store)
+        return bot
 
     def get_customer_from_message(self, message: SignalMessage) -> Customer:
         customer, _ = Customer.objects.get_or_create(phone_number=message.source)
         return customer
 
-    def log_and_send_message(self, customer: Customer, text: str, session: MobotChatSession):
-        sent_message = Message(
-            customer=customer,
-            store=Mobot.store,
-            text=text,
-            chat_session=session,
-            direction=MessageDirection.MESSAGE_DIRECTION_SENT)
-        sent_message.save()
-        self.signal.send_message(str(customer.phone_number), text)
-
-    def register_handler(self, regex: str, method: Callable[[SignalMessage], Any], order: int = 100):
+    def register_handler(self, regex: str, method: Callable[[Message], Any], order: int = 100):
         if not isinstance(regex, RE_TYPE):
             regex = re.compile(regex, re.I)
         self._chat_handlers.append((order, regex, method))
         # Use only the first value to sort so that declaration order doesn't change.
         self._chat_handlers.sort(key=lambda x: x[0])
 
-    def unsubscribe_handler(self, message: SignalMessage):
-        customer = self.get_customer_from_message(message)
-        store_preferences = self.get_customer_store_preferences(customer)
-
-        if not store_preferences.allows_contact:
-            self.log_and_send_message(customer, message.source, NOT_RECEIVING_NOTIFICATIONS)
+    def unsubscribe_handler(self, context: MessageContextBase):
+        if not context.store_preferences.allows_contact:
+            # User already inactive
+            context.log_and_send_message(ChatStrings.NOT_RECEIVING_NOTIFICATIONS)
         else:
-            store_preferences.allows_contact = False
-            store_preferences.save()
+            context.store_preferences.allows_contact = False
+            context.store_preferences.save()
 
-            self.log_and_send_message(customer, message.source,
-                                      NO_INFO_FUTURE_DROPS)
+            context.log_and_send_message(ChatStrings.NO_INFO_FUTURE_DROPS)
 
-    def privacy_policy_handler(self, message: SignalMessage):
-        self.log_and_send_message(message.source, self.store.privacy_policy_url)
+    def privacy_policy_handler(self, context: MessageContextBase):
+        context.log_and_send_message(self.store.privacy_policy_url)
         return
 
     def set_customer_preferences(self, customer: Customer, allow_contact: bool) -> CustomerStorePreferences:
@@ -100,22 +95,15 @@ class Mobot:
         customer_prefs.save()
         return customer_prefs
 
-    def find_active_drop_for_customer(self, customer: Customer) -> Set[DropSession]:
-        return set(DropSession.objects.filter(campaign=self.campaign, customer=customer).all())
+    def handle_greet_customer(self, context: MessageContextBase):
+        greeting = ChatStrings.GREETING.format(name=self.name, store=self.store, message_text=context.message.text)
+        context.log_and_send_message(greeting)
 
-    def get_chat_session_with_customer(self, customer: Customer) -> MobotChatSession:
-        chat_session, _ = MobotChatSession.objects.get_or_create(mobot=self.mobot, customer=customer)
-        return chat_session
+    def handle_start_conversation(self, context: MessageContextBase):
+        if not self.campaign.is_active():
+            context.log_and_send_message(ChatStrings.CAMPAIGN_INACTIVE)
+        else:
 
-    def handle_greet_customer(self, message: SignalMessage):
-        customer = self.get_customer_from_message(message)
-        greeting = GREETING.format(name=self.name, store=self.store, message_text=message.text)
-        self.log_and_send_message(customer, greeting)
-
-    def handle_start_conversation(self, message: SignalMessage):
-        customer = self.get_customer_from_message(message)
-        customer_store_preferences = self.get_customer_store_preferences(customer)
-        chat_session = self.get_chat_session_with_customer(customer=customer)
 
     def _handle_chat(self, message: SignalMessage):
         for _, regex, func in self._chat_handlers:
@@ -123,38 +111,30 @@ class Mobot:
             if not match:
                 continue
             try:
-                return func(message)
+                with self.get_context_from_message(message) as context:
+                    return func(context)
             except Exception as e:  # noqa - We don't care why this failed.
                 self.logger.error(e)
                 continue
 
-    def _save_message(self, message: SignalMessage):
-        customer = self.get_customer_from_message(message)
-        chat_session = self.get_chat_session_with_customer(customer)
-        logged_message = Message(
-            direction=MessageDirection.MESSAGE_DIRECTION_RECEIVED,
-            customer=customer,
-            text=message.text,
-            chat_session=chat_session,
-        )
-
-
-    def _process_message(self, message: SignalMessage):
-        customer = self.get_customer_from_message(message)
-        chat_session = self.get_chat_session_with_customer(customer=customer)
-        Message.objects.create(customer=customer, )
-        self._handle_chat(message)
-
     def register_handlers(self):
-        self.register_handler("unsubscribe", Mobot.unsubscribe_handler)
+        self.register_handler("unsubscribe", self.unsubscribe_handler)
         self.register_handler("", self._greet_customer)
         self.register_handler("p", self.privacy_policy_handler)
         Mobot.signal.payment_handler(self.handle_payment)
 
+    def find_and_greet_targets(self):
+        for customer in self.campaign.get_target_customers():
+            ctx = self.get_context_from_customer(customer)
+            if ctx.drop_session.state == DropSession.State.CREATED:
+                ctx.log_and_send_message(ChatStrings.GREETING.format(store=self.store,
+                                                                     campaign=self.campaign,
+                                                                     campaign_description=self.campaign.description))
+
     def run(self):
-        self.signal.register_subscriber(self._per_cust_subscriber)
+        self.signal.register_subscriber(self._subscriber)
         with ThreadPoolExecutor(4) as executor:
             self._executor_futures.append(executor.submit(self.signal.run_chat, True))
             for message in self._subscriber.receive_messages():
-                self._executor_futures.append(executor.submit(self._process_message, message))
+                self._executor_futures.append(executor.submit(self._handle_chat, message))
         executor.shutdown(wait=True)
