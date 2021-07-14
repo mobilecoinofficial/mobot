e56716b74d87ca9f6e41da81c3432b9d61d57701
Greg Rice <gregrice@gmail.com>
2021-07-14 16:35:29 -0700

    Revert "Revert "Things are getting weird, let's put a quick commit down""
    This reverts commit 85995886a8bd8fe3213f6e4c231386d57c14ca68.

--- mobot/apps/chat/chat_strings.py
+++ mobot/apps/chat/chat_strings.py
@@ -1,4 +1,5 @@
 from string import Template
+from mobot.apps.merchant_services.models import Customer, Campaign, Order
 
 class ChatStrings:
     GREETING = """
@@ -21,6 +22,8 @@ class ChatStrings:
     CAMPAIGN_INACTIVE = "Hi! MOBot here.\n\n We're currently closed. Buh-Bye!"
     NO_INFO_FUTURE_DROPS = "You will no longer receive notifications about future drops."
     NOT_RECEIVING_NOTIFICATIONS = "You are not currently receiving any notifications"
+    SUBSCRIBED_ALREADY = "You are already subscribed to receive notifications from this store! Thanks!"
+    SUBSCRIBED_FIRST_TIME = "Thanks, we'll let you know about future drops from this store!"
     NOT_VALID_FOR_CAMPAIGN = "Sorry, the current campaign requires a number with a country code of {country_code}"
     DIDNT_UNDERSTAND = "Sorry, I didn't understand that."
 
@@ -35,3 +38,6 @@ class ChatStrings:
         
         "buy 123"
     """
+    @staticmethod
+    def offer_text(customer: Customer, campaign: Campaign) -> str:
+        return f"Hey {customer.name} We're happy to offer you the following campaign: {campaign.description}"
--- mobot/apps/payment_service/service.py
+++ mobot/apps/payment_service/service.py
@@ -1,10 +1,17 @@
-import mobilecoin
+import logging
+import time
+
 from typing import Protocol, NewType
 from dataclasses import dataclass
 from dacite import from_dict
+from concurrent.futures import ThreadPoolExecutor
+from django.conf import settings
+
+import mobilecoin
 
 # Type aliases
 from mobot.apps.payment_service.models import *
+
 
 AccountId = NewType('AccountId', str) # Full service account ID
 PaymentAmount = NewType('PaymentAmount', float)
@@ -20,39 +27,71 @@ class TransactionLog:
     completed: bool
     receipt: Receipt
 
+def _signald_to_fullservice(r):
+    return {
+        "object": "receiver_receipt",
+        "public_key": r['txo_public_key'],
+        "confirmation": r['txo_confirmation'],
+        "tombstone_block": str(r['tombstone']),
+        "amount": {
+            "object": "amount",
+            "commitment": r['amount_commitment'],
+            "masked_value": str(r['amount_masked'])
+        }
+    }
 
-class PaymentService(Protocol):
-    client: mobilecoin.Client
+class TransactionTimeoutException(Exception):
+    pass
 
-    # def submit_payment_intent(self, account_id: AccountId, amount: PaymentAmount, to_address: Address) -> Payment:
-    #     transaction_log = from_dict(TransactionLog, data=self.client.build_and_submit_transaction(account_id, amount, to_address))
-    #     transaction = Transaction(transaction_id=transaction_log.txo_id)
-    #     transaction.save()
-    #     payment = Payment(transaction=transaction)
-    #     payment.save()
-    #     return payment
+class PaymentService:
+
+    def __init__(self, mobilecoin_client: mobilecoin.Client):
+        self.mobilecoin_client = mobilecoin_client
+        self.executor = ThreadPoolExecutor(4)
+        self._futures = []
+        self.logger = logging.getLogger("PaymentService")
+
+    def submit_payment_intent(self, account_id: AccountId, amount: PaymentAmount, to_address: Address) -> Payment:
+        transaction_log = from_dict(TransactionLog, data=self.client.build_and_submit_transaction(account_id, amount, to_address))
+        transaction = Transaction(transaction_id=transaction_log.txo_id)
+        transaction.save()
+        payment = Payment(transaction=transaction)
+        payment.save()
+        return payment
 
     def get_payment_status(self, payment: Payment) -> Payment:
         ## TODO: Brian: Help with format of this returned object
         txo: Transaction = self.client.check_receiver_receipt_status(address=Address, receipt=payment.transaction.receipt)
 
+    def get_payment_result(self, transaction: Transaction, max_time_secs: float = 30) -> Payment:
+        start_time = time.time()
+        time_elapsed = 0
+        attempts = 0
+        try:
+            while transaction.transaction_status == transaction.Status.TransactionPending:
+                time_elapsed = time.time() - start_time
+                if time_elapsed > max_time_secs:
+                    raise TransactionTimeoutException(f"Transaction polling lasted longer than max time {max_time_secs}")
+                try:
+                    self.client.poll_txo(transaction.transaction_id)
+                except Exception as e:
+                    transaction.transaction_status = transaction.Status.Other
+                    self.logger.exception("TxOut did not land yet")
+                    attempts += 1
+                    time.sleep(attempts * 0.5)
+                finally:
+                    transaction.save()
+        except TransactionTimeoutException as e:
+            self.logger.exception(f"Failed to get payment result of {transaction} before timeout.")
 
-    ## All following methods are private to Payment Service
     def submit_payment_to_user(self, account_id: AccountId, amount_in_mob: float, customer_payments_address: Address) -> Transaction:
         tx_proposal = self.client.build_transaction(account_id, amount_in_mob, customer_payments_address)
         txo_id = self._submit_transaction(tx_proposal, account_id)
         receipt = self._create_receiver_receipt(tx_proposal)
         transaction = Transaction(transaction_id=txo_id, transaction_amt=amount_in_mob, receipt=receipt)
 
-        # Todo: Extract the polling
-        try:
-            self.client.poll_txo(txo_id)
-            transaction.transaction_status = transaction.Status.TransactionSuccess
-        except Exception:
-            transaction.transaction_status = transaction.Status.Other
-            print("TxOut did not land yet, id: " + txo_id)
-        finally:
-            transaction.save()
+        result_future = self.executor.submit(self.get_payment_result, transaction)
+        self._futures.append(result_future)
 
     def _create_receiver_receipt(self, tx_proposal) -> Receipt:
         receiver_receipts = self.client.create_receiver_receipts(tx_proposal)
