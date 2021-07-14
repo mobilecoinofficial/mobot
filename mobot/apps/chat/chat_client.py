9b611f1d64a868201e9b767f2b0e5336a294ba8c
Greg Rice <gregrice@gmail.com>
2021-07-14 15:15:59 -0700

    Things are getting weird, let's put a quick commit down

--- mobot/apps/chat/chat_client.py
+++ mobot/apps/chat/chat_client.py
@@ -1,32 +1,29 @@
+import re
 import logging
 from enum import Enum
 from concurrent.futures import ThreadPoolExecutor
-from typing import Callable, Any
-from .handlers import *
-import re
+from typing import Callable, Any, Optional
 from typing import Set
 
-import phonenumbers
 import mobilecoin
-import pytz
 
 from django.conf import settings
 from django.utils import timezone
 
-
-from .models import Message, MessageDirection, MobotBot, MobotChatSession
+from .models import MobotBot, MobotChatSession
 from mobot.apps.merchant_services.models import Customer, CustomerStorePreferences, DropSession, Campaign, Store
 from mobot.signald_client import Signal
 from mobot.lib.signal import SignalCustomerDataClient
 from mobot.signald_client.types import Message as SignalMessage
 from mobot.signald_client.main import QueueSubscriber
 from mobot.apps.chat.context import MessageContextFactory, MessageContextBase
+from .handlers import *
+
 
 class TransactionStatus(str, Enum):
     TRANSACTION_SUCCESS = "TransactionSuccess"
     TRANSACTION_PENDING = "TransactionPending"
 
-RE_TYPE = type(re.compile(""))
 
 class MobotMessage(str, Enum):
     YES = "y"
@@ -37,20 +34,21 @@ class MobotMessage(str, Enum):
 
 class MobotHandler:
     def __init__(self,
-                 regex,
                  method: Callable[[MessageContextBase], Any],
+                 context_filter: Callable[[MessageContextBase], bool] = None,
                  order: int = 100,
                  drop_session_states: Set[DropSession.State] = set(),
-                 chat_session_states: Set[MobotChatSession.State] = set()):
-        self.regex = regex
+                 chat_session_states: Set[MobotChatSession.State] = set(),
+                 ):
         self._method = method
         self.drop_session_states = drop_session_states
         self.chat_session_states = chat_session_states
         self.order = order
+        self._should_run = context_filter if context_filter else lambda c: True  # If no filter, always run
 
     @property
     def all_states(self):
-        return not self.drop_session_states and not self.chat_session_states # empty states
+        return not self.drop_session_states and not self.chat_session_states  # empty states
 
     def matches_context_states(self, context) -> bool:
         if self.all_states:
@@ -66,7 +64,8 @@ class MobotHandler:
         return False
 
     def handle(self, context: MessageContextBase):
-        return self._method(context)
+        if self._should_run(context):
+            return self._method(context)
 
 
 class Mobot:
@@ -81,7 +80,6 @@ class Mobot:
         self.message_context_factory = MessageContextFactory(self.mobot, self.logger, self.signal)
         self.customer_data_client = SignalCustomerDataClient(signal=self.signal)
         self._subscriber = QueueSubscriber(self.name)
-        self.signal.register_subscriber(self._subscriber)
         self._executor_futures = []
         self._empty_regex_chat_handlers = []
         self._nonempty_regex_chat_handlers = []
@@ -99,17 +97,37 @@ class Mobot:
         bot, _ = MobotBot.objects.get_or_create(name=f"{self.store.name}-Bot", store=self.store, campaign=self.campaign)
         return bot
 
-    def register_handler(self, regex: str, method: Callable[[MessageContextBase], Any], order: int = 100,
-                               chat_session_states: Set[MobotChatSession.State] = set(),
-                               drop_session_states: Set[DropSession.State] = set()):
-        if regex:
-            if not isinstance(regex, RE_TYPE):
-                regex = re.compile(regex, re.I)
-        handler = MobotHandler(regex, method, order=order, chat_session_states=chat_session_states, drop_session_states=drop_session_states)
-        if handler.regex:
-            self._nonempty_regex_chat_handlers.append(handler)
-        else:
-            self._empty_regex_chat_handlers.append(handler)
+    def _regex_to_filter_func(self, regex: str) -> Callable[[MessageContextBase], bool]:
+        compiled_regex = re.compile(regex)
+
+        def matches(ctx: MessageContextBase):
+            return re.search(compiled_regex, ctx.message.text)
+
+        return matches
+
+    def register_handler(self,
+                         method: Callable[[MessageContextBase], Any],
+                         condition: Optional[Callable[[MessageContextBase], bool]] = None,
+                         order: int = 100,
+                         chat_session_states: Set[MobotChatSession.State] = set(),
+                         drop_session_states: Set[DropSession.State] = set(),
+                         always_run: bool = False,
+                         ):
+        """
+
+        Args:
+            condition:
+            regex: A regex to match message text. Leave blank to match all messages.
+            method: A method that accepts a MessageContext object
+            order: What order to run matched handlers in; defaults to 100. Lower numbers run first.
+            chat_session_states: What ChatSession.State states this should run under
+            drop_session_states: What DropSession.State states this should run under
+            always_run: Normally, handlers with no regex match won't run if there are more specific matches. This sets a
+                bit to always run
+        """
+
+        handler = MobotHandler(regex, method, order=order, chat_session_states=chat_session_states,
+                               drop_session_states=drop_session_states, always_run=always_run)
 
     def set_customer_preferences(self, customer: Customer, allow_contact: bool) -> CustomerStorePreferences:
         customer_prefs = CustomerStorePreferences.objects.get_or_create(customer=customer, store=self.store)
@@ -136,27 +154,36 @@ class Mobot:
                     if handler.matches_context_states(context):
                         matching_handlers.append(handler)
             # Run all handlers in order
-            matching_handlers.sort(key=lambda handler: handler.order)
+            matching_handlers.sort(key=lambda matched_handler: matched_handler.order)
             for handler in matching_handlers:
+
                 try:
                     self.logger.debug(f"Attempting to handle {context.message}")
                     handler.handle(context)
-                except Exception as e:
+                except Exception:
                     self.logger.exception(f"Failed to run handler for {context.message}")
 
     def register_handlers(self):
         self.register_handler("^(u|unsubscribe)$", unsubscribe_handler)
-        self.register_handler("", handle_greet_customer, chat_session_states={MobotChatSession.State.NOT_GREETED}, order=1) # First, say hello to the customer
-        self.register_handler("", handle_start_conversation, chat_session_states={MobotChatSession.State.NOT_GREETED}, order=2) # Then, handle setting up drop session
-        self.register_handler("", handle_already_greeted, chat_session_states={MobotChatSession.State.INTRODUCTION_GIVEN})
+        self.register_handler("^(s|subscribe)$", subscribe_handler)
+        self.register_handler("", handle_greet_customer, chat_session_states={MobotChatSession.State.NOT_GREETED},
+                              order=1)  # First, say hello to the customer
+        self.register_handler("", handle_start_conversation, chat_session_states={MobotChatSession.State.NOT_GREETED},
+                              order=2)  # Then, handle setting up drop session
+        self.register_handler("", handle_already_greeted,
+                              chat_session_states={MobotChatSession.State.INTRODUCTION_GIVEN})
         self.register_handler("", handle_drop_expired, drop_session_states={DropSession.State.EXPIRED})
         self.register_handler("", handle_drop_not_ready, drop_session_states={DropSession.State.NOT_READY})
+        self.register_handler("", handle_no_handler_found)
         self.register_handler("^p$", privacy_policy_handler)
-        self.register_handler("^(i|inventory)$", inventory_handler, drop_session_states={DropSession.State.ACCEPTED, DropSession.State.CREATED, DropSession.State.OFFERED})
+        self.register_handler("^(i|inventory)$", inventory_handler,
+                              drop_session_states={DropSession.State.ACCEPTED, DropSession.State.OFFERED})
 
     def find_and_greet_targets(self, campaign):
         for customer in self.campaign.get_target_customers():
-            CustomerStorePreferences.objects.filter(customer=customer, store_ref=campaign.store)
+            preferences, created = CustomerStorePreferences.objects.get_or_create(customer=customer,
+                                                                                  store_ref=campaign.store)
+
             self.logger.info("Reaching out to existing customers if they pass target validation")
             ctx = self.get_context_from_customer(customer)
             if ctx.drop_session.state == DropSession.State.CREATED:
