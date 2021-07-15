eb37a6ae404aed1c74b8081f88e41399aec506e5
Greg Rice <gregrice@gmail.com>
2021-07-15 14:29:53 -0700

    No tests yet, but set_up_drop command is on its way

--- mobot/apps/drop/management/commands/set_up_drop.py
+++ mobot/apps/drop/management/commands/set_up_drop.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from typing import Dict, Iterable
+from typing import Dict, Iterable, Optional
 import phonenumbers
 from phonenumbers import PhoneNumber
 from mobot.lib.currency import *
@@ -19,7 +19,18 @@ class DropManagmentActions(str, Enum):
     ADD_CAMPAIGN = 'add-campaign'
 
 class Command(BaseCommand):
-    help = 'Add inventory, drops, etc.'
+    help = """
+        Manage inventory for a drop. requires:
+         --action: DropManagementAction, eg add-inventory
+         --store-phone-number: PhoneNumber
+         --product-name: name of the product - think 'MobileCoin Hoodie'
+         
+         Inventory objects look like this: '{"S":10,"M":15,"L":12,"XL":10,"XXL":10}' and are parsed from a json
+         string against mobot.campaigns.hoodies.Size enum.
+         
+         Probably won't handle a price adjustment well, as this expects the price to remain the same once created.
+         TODO: @greg fix this in the future.
+        """
     def __init__(self, *args, **kwargs):
         self.product: Product = None
         self.store: Store = None
@@ -39,6 +50,14 @@ class Command(BaseCommand):
         parser.add_argument('--currency', default=GBP)
 
     def _load_sizes(self, inventory: Dict[str, int]) -> Dict[Size, int]:
+        """
+
+        Args:
+            inventory: A dictionary of size strings and the amount available
+
+        Returns:
+            dictionary of Size enums mapped to amount available
+        """
         sized = dict()
         for key, value in inventory.items():
             sized[Size(key)] = value
@@ -55,7 +74,7 @@ class Command(BaseCommand):
     def add_products(self, product_group_name: str, inventory: Dict[Size, int]):
         pass
 
-    def _add_sized_product(self, product_group: ProductGroup, size: str, price: Money = Money(Decimal(25.0), currency=GBP)) -> Product:
+    def get_or_add_sized_product(self, product_group: ProductGroup, size: str, price: Money = Money(Decimal(25.0), currency=GBP)) -> Product:
         hoodie_product, created = Product.objects.get_or_create(
             name=f"Hoodie Size {size}",
             price=price,
@@ -67,7 +86,7 @@ class Command(BaseCommand):
         return hoodie_product
 
     def handle(self, *args, **options):
-        self.inventory = self._load_sizes(options.get('inventory'))
+        self.inventory = self._load_sizes(options['inventory'])
         self.product_name = options.get('product_name')
         self.store_name = options.get('store_name')
         self.store_phone_number = options.get('store_phone_number')
@@ -75,7 +94,8 @@ class Command(BaseCommand):
         self.store = None
         self.product = None
         price = options.get('price')
-        currency = options.get('currency')
+        currency = options['currency'] # safe as it has a default
+        price_with_currency: Optional[Money] = None if not price else Money(price, currency)
 
         if self.action == DropManagmentActions.CREATE_STORE:
             self.store = self.add_or_get_store(self.store_phone_number, self.store_name)
@@ -83,15 +103,16 @@ class Command(BaseCommand):
         if self.action == DropManagmentActions.ADD_PRODUCT:
             self.product, created = ProductGroup.objects.get_or_create(name=self.product_name)
 
-        if self.action == DropManagmentActions.UPDATE_INVENTORY:
+        if self.action in (DropManagmentActions.UPDATE_INVENTORY, DropManagmentActions.ADD_INVENTORY):
             if not all(self.product_name, price, currency):
-                raise CommandError("Lacking product name; can't create product")
+                raise CommandError("Lacking product inf")
             else:
                 self.product_group = ProductGroup.objects.get(name=self.product_name)
                 for size, amt_available in self.inventory:
-                    price = Money(options.get('price'), options.get('currency'))
-                    product = self._add_sized_product(product_group=self.product_group, price=price, size=size)
-                    product.add_inventory(amt_available)
+                    product = self.get_or_add_sized_product(product_group=self.product_group, price=price_with_currency, size=size)
+                    if self.action == DropManagmentActions.ADD_INVENTORY:
+                        product.add_inventory(amt_available)
+
 
 
 
