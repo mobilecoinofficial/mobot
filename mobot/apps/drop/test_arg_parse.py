23e4777073fea70b287d6fd66da27989affcf975
Greg Rice <gregrice@gmail.com>
2021-07-15 14:00:40 -0700

    Add some management features

--- mobot/apps/drop/test_arg_parse.py
+++ mobot/apps/drop/test_arg_parse.py
@@ -1,6 +1,8 @@
 import argparse
 import json
 import phonenumbers
+from mobot.lib.currency import *
+from decimal import Decimal
 from mobot.campaigns.hoodies import Size
 from typing import Dict
 
@@ -12,13 +14,15 @@ def load_sizes(inventory: Dict[str, int]) -> Dict[Size, int]:
     return sized
 
 def add_arguments(parser: argparse.ArgumentParser):
-    parser.add_argument('-n', '--store-phone-number', type=phonenumbers.parse, required=True)
+    parser.add_argument('-n', '--store-phone-number', type=phonenumbers.parse, required=False)
     parser.add_argument('-s', '--store-name', type=str, required=False)
     parser.add_argument('-p', '--product-name', type=str, help="Name of the product group", default="Hoodie")
     parser.add_argument('-i', '--inventory',
                         type=json.loads,
                         help="JSON of the inventory for the product at various sizes; e.g '{'S': 10, 'M': 10, 'L': 10 }'",
                         default='{}')
+    parser.add_argument('--price', type=Decimal, required=False, default=Decimal(10.0))
+    parser.add_argument('--currency', type=Currency, default=GBP)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -26,3 +30,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     args.inventory = load_sizes(args.inventory)
     print(args.inventory)
+    print(Money(args.price, args.currency))
