6b99752a9baa1de9d0763dee95164292c216adb9
Greg Rice <gregrice@gmail.com>
2021-07-16 10:55:23 -0700

    Clean up for external use

--- mobot/apps/drop/management/commands/__init__.py
+++ mobot/apps/drop/management/commands/__init__.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from mobot.apps.merchant_services.models import Campaign, Product, ProductGroup, Store, Merchant, Order, InventoryItem, Validation, Customer
+from mobot.apps.merchant_services.models import Campaign, Product, ProductGroup, MobotStore, Merchant, Order, InventoryItem, Validation, Customer
 from mobot.campaigns.hoodies import Size
 from mobot.lib.currency import MOB, PMOB
 
