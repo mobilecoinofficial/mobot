3e4d64b4e85b32aa37773f65f934807efe4b13a1
sugargoat <sugargoat@mobilecoin.com>
2021-07-17 15:31:15 -0700

    Copyright and local /usr/bin/ path situation

--- host_machine_scripts/rebuild-docker-containers.sh
+++ host_machine_scripts/rebuild-docker-containers.sh
@@ -3,15 +3,19 @@
 CACHE=$1
 echo "Cache set to $CACHE"
 
+# FIXME make sure /usr/local/bin is in the PATH
+PATH=$PATH:/usr/local/bin
+
+echo "MY PATH" $PATH
 set -e
-VENV=$(pipenv --venv)
+VENV=$(/usr/local/bin/pipenv --venv)
 source ${VENV}/bin/activate
-pipenv install
+/usr/local/bin/pipenv install
 CACHEBUST=$(date +%s)
 
 if [[ "${CACHE:-true}" == true ]]
 then
-  docker-compose build --build-arg CACHEBUST=$CACHEBUST
+  /usr/local/bin/docker-compose build --build-arg CACHEBUST=$CACHEBUST
 else
-  docker-compose build --no-cache --build-arg CACHEBUST=$CACHEBUST
+  /usr/local/bin/docker-compose build --no-cache --build-arg CACHEBUST=$CACHEBUST
 fi
