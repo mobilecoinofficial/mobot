af85f8e7f29aff25c2fef40651d21edd4483946a
Brian Corbin <brian.william.corbin@gmail.com>
2021-07-29 20:12:24 -0400

    added functionality for item sizes drop

--- mobot/mobot/settings.py
+++ mobot/mobot/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY', "123")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
