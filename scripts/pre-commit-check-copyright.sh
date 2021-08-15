9c216462c426b7e45f5cef6177c3204ab864a509
Greg Rice <gregrice@gmail.com>
2021-08-15 16:43:24 -0700

    Added pre-commit hook to check copyright

--- scripts/pre-commit-check-copyright.sh
+++ scripts/pre-commit-check-copyright.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Running pre-commit hook"
-COPYRIGHT_TEXT="# Copyright (c) 2021 MobileCoin. All rights reserved."
+echo "Running pre-commit hook to check copyright on python files"
+COPYRIGHT_TEXT="Copyright (c) 2021 MobileCoin. All rights reserved."
 
 git diff --cached --name-status | while read flag file; do
-    echo "File: ${file}"
+    if [[ "${flag}" == 'D' ]]; then
+      continue;
+    fi
     EXTENSION=$([[ "$file" = *.* ]] && echo ".${file##*.}" || echo '')
-    echo "Extension: ${EXTENSION}"
-    if [[ "${EXTENSION}" == ".py" && ! $(head -1 "${file}" | grep -q "${COPYRIGHT_TEXT}") ]]; then
+    if [[ "${EXTENSION}" == ".py" ]]; then
+      if [[ $(grep "${COPYRIGHT_TEXT}" "${file}") == '' ]]; then
         echo "ERROR: Missing MobileCoin Copyright in file: ${file}" >&2
         exit 1
+      fi
     fi
 done
