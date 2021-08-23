647db42bdf518eb02918f7e8cb5d28edbc196288
Greg Rice <gregrice@gmail.com>
2021-08-23 09:08:55 -0700

    Model tests

---
+++ src/mobot_client/tests/drop_session.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+from mobot_client.air_drop_session import AirDropSession
+
+
+class AirDropSessionTests(TestCase):
+    def test_airdrop(self):
+        self.assertEqual(True, False)
+
+
+
