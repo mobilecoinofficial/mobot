4178d293062cf9879aa138f60a9e88cc3a3bba7d
Greg Rice <gregrice@gmail.com>
2021-08-23 16:10:45 -0700

    Ignore manage.py and deleted files

--- scripts/pre-commit-check-copyright.sh
+++ scripts/pre-commit-check-copyright.sh
@@ -2,11 +2,19 @@
 
 echo "Running pre-commit hook to check copyright on python files"
 COPYRIGHT_TEXT="Copyright (c) 2021 MobileCoin. All rights reserved."
+IGNORE="src/manage.py"
 
 git diff --cached --name-status | while read flag file; do
     if [[ "${flag}" == 'D' ]]; then
       continue;
     fi
+    if [[ ! -f "${file}" ]]; then
+      continue
+    fi
+    if [[ "${file}" == "${IGNORE}" ]]; then
+      continue
+    fi
+    
     EXTENSION=$([[ "$file" = *.* ]] && echo ".${file##*.}" || echo '')
     if [[ "${EXTENSION}" == ".py" ]]; then
       if [[ $(grep "${COPYRIGHT_TEXT}" "${file}") == '' ]]; then
