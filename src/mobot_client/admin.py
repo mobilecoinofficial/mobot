707dc5d11c74c84d2e01ab5f94015e954c70b44e
Greg Rice <gregrice@gmail.com>
2021-09-03 10:52:01 -0700

    Inline administration of bonus coins in drop

--- src/mobot_client/admin.py
+++ src/mobot_client/admin.py
@@ -48,28 +48,6 @@ class CustomerAdmin(admin.ModelAdmin):
             return session.get_state_display()
 
 
-class DropAdmin(admin.ModelAdmin):
-    list_display = ('name', 'store', 'is_active', 'initial_sent', 'bonus_payments', 'total_spent',)
-    readonly_fields = ('initial_coin_limit', 'is_active', 'initial_coins_available', 'bonus_coins_available_display', 'total_spent')
-
-    @admin.display(description='Bonus Coins')
-    def bonus_coins_available_display(self, obj):
-        return "\n".join([f"{pmob2mob(coin.amount_pmob):.3f} MOB : ({coin.number_remaining()}/{coin.number_available_at_start}) available" for coin in obj.bonus_coins.all()])
-
-    @admin.display(description='Initial Payments')
-    def initial_sent(self, obj):
-        return obj.num_initial_sent()
-
-    @admin.display(description='Bonus Payments')
-    def bonus_payments(self, obj):
-        return obj.num_bonus_sent()
-
-    @admin.display(description='Total Spent (MOB)')
-    def total_spent(self, obj):
-        return pmob2mob(obj.total_pmob_spent())
-
-
-
 class ItemAdmin(admin.ModelAdmin):
     pass
 
@@ -102,7 +80,6 @@ class BonusCoinAdmin(admin.ModelAdmin):
     def amount_mob(self, obj):
         return f"{pmob2mob(obj.amount_pmob):.4f}"
 
-
 class SkuAdmin(admin.ModelAdmin):
     readonly_fields = ('number_available',)
 
@@ -120,10 +97,41 @@ class ChatbotSettingsAdmin(admin.ModelAdmin):
         return ChatbotSettings.objects.all().count() < 1
 
 
+class BonusCoinInline(admin.TabularInline):
+    model = BonusCoin
+    readonly_fields = ('number_claimed', 'number_remaining',)
+
+
+class DropAdmin(admin.ModelAdmin):
+    inlines = [
+        BonusCoinInline
+    ]
+    list_display = ('name', 'store', 'is_active', 'initial_sent', 'bonus_payments', 'total_spent',)
+    readonly_fields = ('initial_coin_limit', 'is_active', 'initial_coins_available', 'bonus_coins_available_display', 'total_spent')
+
+    @admin.display(description='Bonus Coins')
+    def bonus_coins_available_display(self, obj):
+        return "\n".join([f"{pmob2mob(coin.amount_pmob):.3f} MOB : ({coin.number_remaining()}/{coin.number_available_at_start}) available" for coin in obj.bonus_coins.all()])
+
+    @admin.display(description='Initial Payments')
+    def initial_sent(self, obj):
+        return obj.num_initial_sent()
+
+    @admin.display(description='Bonus Payments')
+    def bonus_payments(self, obj):
+        return obj.num_bonus_sent()
+
+    @admin.display(description='Total Spent (MOB)')
+    def total_spent(self, obj):
+        return pmob2mob(obj.total_pmob_spent())
+
+
+
 admin.site.register(Store, StoreAdmin)
 admin.site.register(Customer, CustomerAdmin)
 admin.site.register(Drop, DropAdmin)
 admin.site.register(Item, ItemAdmin)
+admin.site.register(ChatbotSettings, ChatbotSettingsAdmin)
 admin.site.register(CustomerStorePreferences, CustomerStorePreferencesAdmin)
 admin.site.register(CustomerDropRefunds, CustomerDropRefundsAdmin)
 admin.site.register(DropSession, DropSessionAdmin)
@@ -131,4 +139,3 @@ admin.site.register(Message, MessageAdmin)
 admin.site.register(BonusCoin, BonusCoinAdmin)
 admin.site.register(Sku, SkuAdmin)
 admin.site.register(Order, OrderAdmin)
-admin.site.register(ChatbotSettings, ChatbotSettingsAdmin)
