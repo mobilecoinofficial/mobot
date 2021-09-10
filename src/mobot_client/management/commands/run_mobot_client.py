e901517113ee08834201acbbf9fcbbc157fcd646
Greg Rice <gregrice@gmail.com>
2021-09-10 10:50:03 -0700

    Wait until CB settings to start MOBot

--- src/mobot_client/management/commands/run_mobot_client.py
+++ src/mobot_client/management/commands/run_mobot_client.py
@@ -3,7 +3,7 @@
 """
 The entrypoint for the running MOBot.
 """
-
+import time
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
@@ -18,6 +18,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         cb_settings = ChatbotSettings.load()
+        while not cb_settings.store:
+            print("Awaiting settings... sleeping 5 seconds! Create a store and attach to ChatbotSettings to continue.")
+            time.sleep(5)
+            cb_settings.refresh_from_db()
         store = cb_settings.store
         bot_avatar_filename = cb_settings.avatar_filename
         bot_name = cb_settings.name
