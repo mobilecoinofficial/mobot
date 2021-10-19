13b1b88d4eefc30a145d6b40fce51e130f8a2550
Greg Rice <gregrice@gmail.com>
2021-10-19 13:40:24 -0500

    Change so this command actually works

--- scripts/watch_and_run.sh
+++ scripts/watch_and_run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -x
 LOGFILE=$1
 MATCH=$2
-CMD="signaldctl key trust-all"
+CMD='signaldctl key trust-all -a +447401150902'
 echo "Watching logfile $LOGFILE for match $MATCH and running $CMD"
 tail -fn0 $LOGFILE | \
 while read line ; do
